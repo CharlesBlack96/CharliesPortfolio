4a1ec09e1836cd48c07f0e1c03b7cae128dd4bb2
Charlie Black <charlieblack@Larrys-MacBook-Pro.local>
2022-04-22 14:41:38 -0700

    real sentence in p tag

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,11 +6,7 @@ const Contact = () => {
     <div>
       <Navbar />
       <h1>Why You Should Contact Me</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore,
-        asperiores. Nihil, dolorum nostrum facilis dolorem aliquam ex quaerat
-        deleniti eius odio id eveniet corrupti et tenetur aliquid ipsum ab!
-      </p>
+      <p>I am changing this text to a real sentence.</p>
     </div>
   );
 };
